814abdba21d922cd4368f3602bb6e664f3f83c11
Alex Adriano <alexadriano@gmail.com>
2018-07-22 08:11:30 -0300

    Arquivos

--- ProductionMaker/ProductionMaker/Main.Designer.cs
+++ ProductionMaker/ProductionMaker/Main.Designer.cs
@@ -37,6 +37,12 @@
             this.button3 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Encoding = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tamanho = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.ckbcaminho = new System.Windows.Forms.ComboBox();
             this.ckcIgEncode = new System.Windows.Forms.CheckBox();
@@ -54,12 +60,6 @@
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.SalvarComo = new System.Windows.Forms.FolderBrowserDialog();
-            this.Column4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Encoding = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Tamanho = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.groupBox2.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -172,6 +172,49 @@
             this.dataGridView1.Size = new System.Drawing.Size(968, 286);
             this.dataGridView1.TabIndex = 36;
             // 
+            // Column4
+            // 
+            this.Column4.FillWeight = 30F;
+            this.Column4.HeaderText = "";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 30;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.DataPropertyName = "name";
+            this.Column2.FillWeight = 200F;
+            this.Column2.HeaderText = "Nome";
+            this.Column2.Name = "Column2";
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.DataPropertyName = "FullName";
+            this.Column1.FillWeight = 450F;
+            this.Column1.HeaderText = "Pasta";
+            this.Column1.Name = "Column1";
+            // 
+            // Encoding
+            // 
+            this.Encoding.DataPropertyName = "Encoding";
+            this.Encoding.HeaderText = "Encoding";
+            this.Encoding.Name = "Encoding";
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column3.DataPropertyName = "Data";
+            this.Column3.HeaderText = "Modificado";
+            this.Column3.Name = "Column3";
+            // 
+            // Tamanho
+            // 
+            this.Tamanho.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Tamanho.DataPropertyName = "Tamanho";
+            this.Tamanho.HeaderText = "Tamanho";
+            this.Tamanho.Name = "Tamanho";
+            // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.ckbcaminho);
@@ -192,7 +235,7 @@
             this.groupBox3.Size = new System.Drawing.Size(530, 172);
             this.groupBox3.TabIndex = 38;
             this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Seleção de arquivos";
+            this.groupBox3.Text = "Arquivos";
             // 
             // ckbcaminho
             // 
@@ -333,49 +376,6 @@
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
-            // 
-            // Column4
-            // 
-            this.Column4.FillWeight = 30F;
-            this.Column4.HeaderText = "";
-            this.Column4.Name = "Column4";
-            this.Column4.Width = 30;
-            // 
-            // Column2
-            // 
-            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column2.DataPropertyName = "name";
-            this.Column2.FillWeight = 200F;
-            this.Column2.HeaderText = "Nome";
-            this.Column2.Name = "Column2";
-            // 
-            // Column1
-            // 
-            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column1.DataPropertyName = "FullName";
-            this.Column1.FillWeight = 450F;
-            this.Column1.HeaderText = "Pasta";
-            this.Column1.Name = "Column1";
-            // 
-            // Encoding
-            // 
-            this.Encoding.DataPropertyName = "Encoding";
-            this.Encoding.HeaderText = "Encoding";
-            this.Encoding.Name = "Encoding";
-            // 
-            // Column3
-            // 
-            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column3.DataPropertyName = "Data";
-            this.Column3.HeaderText = "Modificado";
-            this.Column3.Name = "Column3";
-            // 
-            // Tamanho
-            // 
-            this.Tamanho.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Tamanho.DataPropertyName = "Tamanho";
-            this.Tamanho.HeaderText = "Tamanho";
-            this.Tamanho.Name = "Tamanho";
             // 
             // Main
             // 
